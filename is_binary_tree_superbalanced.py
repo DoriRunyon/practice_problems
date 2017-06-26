d5a75fd714216c9b9b7a25bde23880b670d475de
DoriRunyon <dorileerunyon@gmail.com>
2017-06-26 11:08:44 -0700

    Made some fixes and added two tests.

--- is_binary_tree_superbalanced.py
+++ is_binary_tree_superbalanced.py
@@ -1,26 +1,77 @@
-def is_superbalanced(binarytree):
+"""
 
-    leaf_node_present = None
+>>> A = BinaryTreeNode('A')
+>>> B = BinaryTreeNode('B')
+>>> C = BinaryTreeNode('C')
+>>> A.left = B
+>>> A.right = C
 
-    nodes_to_check =[(binarytree, 0)]
+>>> A.is_superbalanced()
+True
 
-    while nodes_to_check:
 
-        current_node = nodes_to_check.pop()
+>>> D = BinaryTreeNode('D')
+>>> E = BinaryTreeNode('E')
+>>> F = BinaryTreeNode('F')
+>>> G = BinaryTreeNode('G')
+>>> H = BinaryTreeNode('H')
+>>> D.left = E
+>>> D.right = F
+>>> E.left = G
+>>> G.left = H
 
-        if leaf_node_present:
-            if current_node[1] - leaf_node_present >= 2:
-                return False
+>>> D.is_superbalanced()
+False
 
-        if not current_node.left and not current_node.right:
-            leaf_node_present = current_node[1]
+"""
 
-        else:
 
-            if current_node[0].left:
-                nodes_to_check.append[(current_node[0].left, current_node[1]+1)]
+#binary tree node class - given by Interview Cake
+class BinaryTreeNode:
 
-            if current_node[0].right:
-                nodes_to_check.append[(current_node[0].right, current_node[1]+1)]
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
 
-    return True
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
+
+
+    #my solution
+    def is_superbalanced(self):
+
+        leaf_node_present = None
+
+        nodes_to_check =[(self, 0)]
+
+        while nodes_to_check:
+
+            current_node = nodes_to_check.pop()
+
+            if leaf_node_present:
+                if current_node[1] - leaf_node_present >= 2:
+                    return False
+
+            if not current_node[0].left and not current_node[0].right:
+                leaf_node_present = current_node[1]
+
+            else:
+
+                if current_node[0].left:
+                    nodes_to_check.append((current_node[0].left, current_node[1]+1))
+
+                if current_node[0].right:
+                    nodes_to_check.append((current_node[0].right, current_node[1]+1))
+
+        return True
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED!\n"
